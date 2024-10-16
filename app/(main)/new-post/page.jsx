0fd6a50e932a31756d9f2b15beec2388dfaae82c
Aysu Aşçı <[aysuacunmedya@gmail.com]>
2024-10-16 18:28:14 +0300

    new-post page.jsx sayfası düzenlendi

--- app/(main)/new-post/page.jsx
+++ app/(main)/new-post/page.jsx
@@ -1,18 +1,29 @@
+import './new-post.css';
 import { SavePost } from "./action";
 
-export default function NewPost () {
-  return(
-    <div>
-      <form action={SavePost}>
-        <input type="text" name="title" placeholder="Yazı Başlığı"/>
-        <br />
-        <textarea name="content" id="" placeholder="Yazı İçeriği"></textarea>
-        < br />
-        <button>Yazıyı Paylaş</button>
+export default function NewPost() {
+  return (
+    <div className="new-post-container">
+      <h2 className="new-post-title">Yeni Yazı Oluştur</h2>
+      <form action={SavePost} method="post">
+        <input 
+          type="text" 
+          name="title" 
+          placeholder="Yazı Başlığı" 
+          className="new-post-input"
+          required
+        />
+        <textarea 
+          name="content" 
+          placeholder="Yazı İçeriği" 
+          rows="10"
+          className="new-post-textarea"
+          required
+        />
+        <button type="submit" className="new-post-button">
+          Yazıyı Paylaş
+        </button>
       </form>
     </div>
-  )
-  // yazılar sayfasını nerede acıcagımızı biz seçicez anasayfada da olabilir yada sayfalar diye ayrı bir sayfamızda olabilir.
-  
-
+  );
 }
