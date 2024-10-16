10ac783ee3e854394fa7092b69805c5738b78257
Aysu Aşçı <[aysuacunmedya@gmail.com]>
2024-10-16 18:25:34 +0300

    new-post action.js sayfası düzenlendi

--- app/(main)/new-post/action.js
+++ app/(main)/new-post/action.js
@@ -1,23 +1,31 @@
-"use server"
+"use server";
+import { supabase } from '../../../utils/supabase/client';
+import { redirect } from 'next/navigation';
+
 export async function SavePost(formData) {
   const title = formData.get("title");
   const content = formData.get("content");
-  const supabse = createClient();
 
-  const { data : {user}} = supabase.auth.getUser();
+  // Kullanıcı bilgilerini aldık
+  const { data: { user } } = await supabase.auth.getUser();
 
-  if(!user) {
-    redirect("/login")
+  if (!user) {
+  
+    redirect("/login");
   }
 
+  // Post verilerini kaydet
   const { data, error } = await supabase
     .from('posts')
-    .insert({ title,content,user_id: user.id})
+    .insert({ title, content, user_id: user.id })
     .select()
-    .single()
+    .single();
 
-    if(error){
-      console.log(errror); 
-    }
-    redirect ('/posts/${data.id}')
+  if (error) {
+    console.log("Error:", error); // error mesajını düzelt
+    return;
+  }
+
+  // Başarılı olursa, postun detay sayfasına yönlendir
+  redirect(`/posts/${data.id}`);
 }
